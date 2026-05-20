387f4ad71a49883ce2a037ff4bcac36b7fd55543
b-tupiti <tupitibrandon@gmail.com>
2026-05-21 00:06:08 +1100

    Move CTA section to just before the footer

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
       <WhyUs />
       <WhoWeAre />
       <FeaturedProjects />
-      <CTA />
       <Publications />
       <LatestArticles />
+      <CTA />
       <Footer />
     </main>
   );
