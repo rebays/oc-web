2ceb211df60454bff204e577521000ce86e54692
b-tupiti <tupitibrandon@gmail.com>
2026-05-20 13:19:07 +1100

    chore: change output to standalone

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // Replace 'your-repo-name' with your actual repository name
-  basePath: '/oc-web', 
-  // This ensures CSS/JS/Images are prefixed correctly
-  assetPrefix: '/oc-web/', 
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
